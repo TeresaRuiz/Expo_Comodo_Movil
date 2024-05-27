085c285476db3b5155f223ee7d188d1b8dc1258e
Allan-fuentes <108203747+Allan-fuentes@users.noreply.github.com>
2024-05-27 14:55:11 -0600

    cambios SecondScreen.js

--- src/screens/SecondScreen.js
+++ src/screens/SecondScreen.js
@@ -1,86 +1,74 @@
-import React from 'react';
-import { View, Text, ScrollView, Image, StyleSheet,StatusBar } from 'react-native';
-import Constants from 'expo-constants';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-const favoriteFoods = [
-  {
-    id: 1,
-    name: 'Tacos',
-    description: 'A dikei le gustan los tacos',
-    image: require('../img/tacos.png'),
-  },
-  {
-    id: 2,
-    name: 'Pupusas',
-    description: 'Pupitas',
-    image: require('../img/pupas.png'),
-  },
-  {
-    id: 3,
-    name: 'Costilla BBQ',
-    description: 'Costilla',
-    image: require('../img/costilla.png'),
-  },
-  {
-    id: 4,
-    name: 'Ramen',
-    description: 'Sopa con verduras',
-    image: require('../img/ramen.png'),
-  },
-  {
-    id: 5,
-    name: 'Penne Pasta',
-    description: 'Le gusta a dikei',
-    image: require('../img/pennePasta.png'),
-  },
-  {
-    id: 6,
-    name: 'Arroz',
-    description: 'Arroz',
-    image: require('../img/arroz.png'),
-  },
-];
-const statusBarHeight = Constants.statusBarHeight || StatusBar.currentHeight || 0;
+const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-const FavoriteFoodsScreen = () => {
+  const handleLogin = () => {
+    // Aquí puedes agregar la lógica para validar las credenciales ingresadas
+    // Por ahora, solo mostraremos las credenciales en la consola para demostración
+    console.log('Nombre de usuario:', username);
+    console.log('Contraseña:', password);
+
+    // Aquí podrías agregar la lógica para iniciar sesión con las credenciales proporcionadas
+  };
+
   return (
-    <ScrollView style={styles.container}>
-      {favoriteFoods.map(food => (
-        <View key={food.id} style={styles.foodContainer}>
-          <Image source={food.image} style={styles.foodImage} />
-          <Text style={styles.foodName}>{food.name}</Text>
-          <Text style={styles.foodDescription}>{food.description}</Text>
-        </View>
-      ))}
-    </ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.title}>Inicio de sesión</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre de usuario"
+        onChangeText={text => setUsername(text)}
+        value={username}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        onChangeText={text => setPassword(text)}
+        value={password}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Iniciar sesión</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop:statusBarHeight,
-    paddingHorizontal:10,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#fff',
+    paddingHorizontal: 20,
   },
-  foodContainer: {
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
     marginBottom: 20,
   },
-  foodImage: {
-    width: '100%',
-    height: 200,
-    resizeMode: 'cover',
-    borderRadius: 8,
+  input: {
+    width: '80%',
+    backgroundColor: '#f0f0f0',
+    height: 50,
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    marginBottom: 15,
   },
-  foodName: {
-    fontSize: 20,
+  button: {
+    backgroundColor: '#007bff',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
     fontWeight: 'bold',
-    marginTop: 10,
-  },
-  foodDescription: {
-    fontSize: 16,
-    marginTop: 5,
   },
 });
 
-export default FavoriteFoodsScreen;
+export default LoginScreen;
