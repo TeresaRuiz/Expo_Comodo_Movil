7962e86ccedd5ff6978cddc78ac3a12a334831fa
JV26vasr <veronicaro3101qr@gmail.com>
2024-06-15 19:20:31 -0600

    cambios finales

--- src/screens/CarritoScreen.js
+++ src/screens/CarritoScreen.js
@@ -17,7 +17,7 @@ const CarritoScreen = ({ navigation }) => {
       title: 'Tenis NIKE',
       description: 'Subtotal',
       image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f37fca8-6bce-43e7-ad07-f57ae3c13142/calzado-air-force-1-07-jBrhbr.png',
-      price: 29.99,
+      price: 69.99,
     },
     
     {
@@ -25,16 +25,16 @@ const CarritoScreen = ({ navigation }) => {
         title: 'Tenis NIKE',
         description: 'Subtotal',
         image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c20afd60-b230-4815-bfd2-6768c875f6cd/calzado-air-force-1-07-J7xw5P.png',
-        price: 29.99,
+        price: 79.99,
       },
       {
         id: '4',
         title: 'Tenis NIKE',
         description: 'Subtotal',
         image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/022c7053-5c55-4bc4-8cdc-72c6e8f95a5e/tenis-air-jordan-1-retro-high-og-latte-Dw2wdP.png',
-        price: 29.99,
+        price: 99.99,
       },
-      
+
   ];
 
   const renderOfertaItem = ({ item }) => (
@@ -48,6 +48,9 @@ const CarritoScreen = ({ navigation }) => {
         <Text style={styles.ofertaTitle}>{item.title}</Text>
         <Text style={styles.ofertaDescription}>{item.description}</Text>
         <View style={styles.ofertaPriceContainer}>
+        <Text style={styles.addButtonText}>+</Text>
+        <Text style={styles.ofertaTitle4}>7</Text>
+        <Text style={styles.addButtonText2}>−</Text>
           <Text style={styles.ofertaPrice}>${item.price.toFixed(2)}</Text>
           {item.discount > 0 && (
             <View style={styles.discountBadge}>
@@ -56,6 +59,9 @@ const CarritoScreen = ({ navigation }) => {
           )}
         </View>
       </View>
+     
+     
+
     </TouchableOpacity>
   );
 
@@ -141,6 +147,30 @@ const CarritoScreen = ({ navigation }) => {
     discountText: {
       fontSize: 12,
       color: '#fff',
+    },
+    addButtonText:{
+
+        borderRadius: 10,
+        width: 30,
+        height:30,
+        color: 'white',
+        textAlign: 'center',
+        backgroundColor: 'black',
+        
+    },
+    addButtonText2:{
+        marginRight: 100,
+        borderRadius: 10,
+        width: 30,
+        height:30,
+        color: 'white',
+        textAlign: 'center',
+        backgroundColor: 'black',
+    },
+    ofertaTitle4:{
+
+        marginRight: 10,
+        marginLeft: 10,
     },
   });
   
