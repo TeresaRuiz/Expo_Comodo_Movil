a0d5468595f3a7975b41cb776e5956bce378f277
dhakhax28 <20220096@ricaldone.edu.sv>
2024-06-15 19:51:46 -0600

    Retoques de historial

--- src/screens/HistorialScreen.js
+++ src/screens/HistorialScreen.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-
 const HistorialScreen = ({ navigation }) => {
   const ofertas = [
     {
@@ -19,41 +18,36 @@ const HistorialScreen = ({ navigation }) => {
       image: 'https://i.pinimg.com/564x/dc/16/1b/dc161b4d8cb4a0df94f6c15b613aaf3d.jpg',
       price: 29.99,
     },
-    
     {
-      id: '2',
+      id: '3',
       title: 'Sandalias Verano Adidas',
       description: 'Cantidad: 1',
       image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c20afd60-b230-4815-bfd2-6768c875f6cd/calzado-air-force-1-07-J7xw5P.png',
       price: 29.99,
     },
-    
     {
-      id: '2',
+      id: '4',
       title: 'Sandalias Verano Adidas',
       description: 'Cantidad: 5',
       image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/022c7053-5c55-4bc4-8cdc-72c6e8f95a5e/tenis-air-jordan-1-retro-high-og-latte-Dw2wdP.png',
       price: 29.99,
     },
-    
     {
-      id: '2',
+      id: '5',
       title: 'Sandalias Verano Adidas',
       description: 'Cantidad. 3',
       image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f37fca8-6bce-43e7-ad07-f57ae3c13142/calzado-air-force-1-07-jBrhbr.png',
       price: 29.99,
     },
-    
     {
-      id: '2',
+      id: '6',
       title: 'Sandalias Verano Adidas',
       description: 'Cantidad: 2',
       image: 'https://originalselsalvador.com/wp-content/uploads/2024/01/calzado-blazer-mid-77-vintage-nw30B2-min.png',
       price: 29.99,
     },
-    
     {
-      id: '2',
+      id: '7',
       title: 'Sandalias Verano Adidas',
       description: 'Cantidad: 1',
       image: 'https://i.pinimg.com/564x/dc/16/1b/dc161b4d8cb4a0df94f6c15b613aaf3d.jpg',
@@ -84,7 +78,7 @@ const HistorialScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-     <Text style={styles.title}>Historial</Text>
+      <Text style={styles.title}>Historial</Text>
       <FlatList
         data={ofertas}
         renderItem={renderOfertaItem}
@@ -94,7 +88,6 @@ const HistorialScreen = ({ navigation }) => {
     </View>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
