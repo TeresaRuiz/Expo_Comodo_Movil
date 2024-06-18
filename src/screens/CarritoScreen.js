1f5969c1c0d9d7d5d21a2943069ecd2199cf33bc
JV26vasr <veronicaro3101qr@gmail.com>
2024-06-17 20:03:30 -0600

    cambio en el carrito que se me olvido xd

--- src/screens/CarritoScreen.js
+++ src/screens/CarritoScreen.js
@@ -9,14 +9,14 @@ const CarritoScreen = ({ navigation }) => {
       id: '1',
       title: 'Tenis NIKE',
       image: 'https://originalselsalvador.com/wp-content/uploads/2024/01/calzado-blazer-mid-77-vintage-nw30B2-min.png',
-      description: 'Subtotal',
+      description: 'Cantidad',
       price: 49.95,
     },
     
     {
       id: '2',
       title: 'Tenis NIKE',
-      description: 'Subtotal',
+      description: 'Cantidad',
       image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f37fca8-6bce-43e7-ad07-f57ae3c13142/calzado-air-force-1-07-jBrhbr.png',
       price: 69.99,
     },
@@ -24,14 +24,14 @@ const CarritoScreen = ({ navigation }) => {
     {
         id: '3',
         title: 'Tenis NIKE',
-        description: 'Subtotal',
+        description: 'Cantidad',
         image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c20afd60-b230-4815-bfd2-6768c875f6cd/calzado-air-force-1-07-J7xw5P.png',
         price: 79.99,
       },
       {
         id: '4',
         title: 'Tenis NIKE',
-        description: 'Subtotal',
+        description: 'Cantidad',
         image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/022c7053-5c55-4bc4-8cdc-72c6e8f95a5e/tenis-air-jordan-1-retro-high-og-latte-Dw2wdP.png',
         price: 99.99,
       },
@@ -52,6 +52,7 @@ const CarritoScreen = ({ navigation }) => {
         <Text style={styles.addButtonText}>+</Text>
         <Text style={styles.ofertaTitle4}>7</Text>
         <Text style={styles.addButtonText2}>−</Text>
+        <Text style={styles.boton}>Comprar</Text>
           <Text style={styles.ofertaPrice}>${item.price.toFixed(2)}</Text>
           {item.discount > 0 && (
             <View style={styles.discountBadge}>
@@ -137,6 +138,8 @@ const CarritoScreen = ({ navigation }) => {
       fontSize: 16,
       fontWeight: 'bold',
       color: '#333',
+      marginTop: -150,
+      marginLeft: -20,
     },
     discountBadge: {
       backgroundColor: '#3046BC',
@@ -173,6 +176,21 @@ const CarritoScreen = ({ navigation }) => {
         marginRight: 10,
         marginLeft: 10,
     },
+    
+  boton: {   
+    backgroundColor: '#3046BC',
+    borderRadius: 10,
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 5,
+    alignItems: 'center',
+    marginTop: 10,
+    marginLeft: -70, // Añade un margen izquierdo negativo para mover el botón a la izquierda
+  },
   });
   
   export default CarritoScreen;
