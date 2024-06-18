103b23a1f43931c65be7858387240f7937d41747
JV26vasr <veronicaro3101qr@gmail.com>
2024-06-17 20:09:07 -0600

    cambio que olvide de historial XD

--- src/screens/HistorialScreen.js
+++ src/screens/HistorialScreen.js
@@ -8,6 +8,7 @@ const HistorialScreen = ({ navigation }) => {
       id: '1',
       title: 'Zapatillas Deportivas Nike',
       description: 'Cantidad : 4',
+      fecha: '16/02/24',
       image: 'https://i.pinimg.com/736x/2a/cf/5f/2acf5f8b73e26f38bc018e0bafb70875.jpg',
       price: 49.95,
     },
@@ -15,6 +16,7 @@ const HistorialScreen = ({ navigation }) => {
       id: '2',
       title: 'Sandalias Verano Adidas',
       description: 'Cantidad: 2',
+      fecha: '16/02/24',
       image: 'https://i.pinimg.com/564x/dc/16/1b/dc161b4d8cb4a0df94f6c15b613aaf3d.jpg',
       price: 29.99,
     },
@@ -22,6 +24,7 @@ const HistorialScreen = ({ navigation }) => {
       id: '3',
       title: 'Sandalias Verano Adidas',
       description: 'Cantidad: 1',
+      fecha: '16/02/24',
       image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c20afd60-b230-4815-bfd2-6768c875f6cd/calzado-air-force-1-07-J7xw5P.png',
       price: 29.99,
     },
@@ -29,6 +32,7 @@ const HistorialScreen = ({ navigation }) => {
       id: '4',
       title: 'Sandalias Verano Adidas',
       description: 'Cantidad: 5',
+      fecha: '16/02/24',
       image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/022c7053-5c55-4bc4-8cdc-72c6e8f95a5e/tenis-air-jordan-1-retro-high-og-latte-Dw2wdP.png',
       price: 29.99,
     },
@@ -36,6 +40,7 @@ const HistorialScreen = ({ navigation }) => {
       id: '5',
       title: 'Sandalias Verano Adidas',
       description: 'Cantidad. 3',
+      fecha: '16/02/24',
       image: 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f37fca8-6bce-43e7-ad07-f57ae3c13142/calzado-air-force-1-07-jBrhbr.png',
       price: 29.99,
     },
@@ -43,6 +48,7 @@ const HistorialScreen = ({ navigation }) => {
       id: '6',
       title: 'Sandalias Verano Adidas',
       description: 'Cantidad: 2',
+      fecha: '16/02/24',
       image: 'https://originalselsalvador.com/wp-content/uploads/2024/01/calzado-blazer-mid-77-vintage-nw30B2-min.png',
       price: 29.99,
     },
@@ -50,6 +56,7 @@ const HistorialScreen = ({ navigation }) => {
       id: '7',
       title: 'Sandalias Verano Adidas',
       description: 'Cantidad: 1',
+      fecha: '16/02/24',
       image: 'https://i.pinimg.com/564x/dc/16/1b/dc161b4d8cb4a0df94f6c15b613aaf3d.jpg',
       price: 29.99,
     },
@@ -64,6 +71,7 @@ const HistorialScreen = ({ navigation }) => {
       <View style={styles.ofertaDetails}>
         <Text style={styles.ofertaTitle}>{item.title}</Text>
         <Text style={styles.ofertaDescription}>{item.description}</Text>
+        <Text style={styles.fecha}>{item.fecha}</Text>
         <View style={styles.ofertaPriceContainer}>
           <Text style={styles.ofertaPrice}>${item.price.toFixed(2)}</Text>
           {item.discount > 0 && (
@@ -129,6 +137,11 @@ const styles = StyleSheet.create({
     color: '#333',
   },
   ofertaDescription: {
+    fontSize: 14,
+    color: '#666',
+    marginBottom: 10,
+  },
+  fecha: {
     fontSize: 14,
     color: '#666',
     marginBottom: 10,
