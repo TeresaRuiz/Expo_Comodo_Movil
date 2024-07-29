562be83980dcda80869e17cbd56ac29e29f8f378
ELGERSON8098 <20220175@ricaldone.edu.sv>
2024-07-29 12:30:48 -0500

    cambios

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,16 +1,19 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Animated, Easing, Alert } from 'react-native';
-import styles from '../estilos/LoginScreenStyles';
+import { Ionicons } from '@expo/vector-icons'; // Import Ionicons
+import styles from '../estilos/LoginScreenStyles'; // Importa los estilos desde un archivo externo
 import Button3 from '../componets/Buttons/Button3';
-import * as Constantes from '../utils/constantes';
+import LogOut from '../componets/LogOut';
+import * as Constantes from '../utils/constantes'; // Importar constantes, asumiendo que tienes IP en un archivo de constantes
 
-const LoginScreen = ({ navigation, route }) => {
-  const ip = Constantes.IP;
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const animatedValue = new Animated.Value(0);
+const LoginScreen = ({ navigation }) => {
+  const ip = Constantes.IP; // Definir IP de la API
+  const [username, setUsername] = useState(''); // Estado para el nombre de usuario
+  const [password, setPassword] = useState(''); // Estado para la contraseña
+  const animatedValue = new Animated.Value(0); // Estado para la animación del logo
 
   useEffect(() => {
+    // Configuración de la animación que se repite en bucle
     const animation = Animated.loop(
       Animated.timing(animatedValue, {
         toValue: 1,
@@ -20,40 +23,45 @@ const LoginScreen = ({ navigation, route }) => {
       })
     );
 
+    // Inicia la animación
     animation.start();
 
+    // Limpia la animación al desmontar el componente
     return () => {
       animation.stop();
     };
   }, []);
 
+  // Interpolación para la animación de traslación vertical del logo
   const translateY = animatedValue.interpolate({
     inputRange: [0, 0.5, 1],
     outputRange: [0, 20, 0],
   });
 
+  // Función para manejar el inicio de sesión
   const handleLogin = async () => {
     try {
       const formData = new FormData();
-      formData.append('UsuarioCliente', username);
-      formData.append('clave', password);
+      formData.append('UsuarioCliente', username); // Añadir nombre de usuario al formulario
+      formData.append('clave', password); // Añadir contraseña al formulario
       
-      const url = `${ip}/Expo_Comodo/api/services/public/cliente.php?action=logIn`;
-      console.log('URL solicitada:', url);
+      const url = `${ip}/Expo_Comodo/api/services/public/cliente.php?action=logIn`; // URL para la solicitud
+      console.log('URL solicitada:', url); // Para verificar la URL
 
+      // Realizar la solicitud a la API
       const response = await fetch(url, {
         method: 'POST',
         body: formData,
       });
 
-      const responseText = await response.text();
+      const responseText = await response.text(); // Obtener la respuesta como texto
 
       try {
-        const data = JSON.parse(responseText);
+        const data = JSON.parse(responseText); // Intenta parsear la respuesta como JSON
         if (data.status) {
-          navigation.navigate('DashboardTabs');
+          navigation.navigate('DashboardTabs'); // Navega a la pantalla de pestañas del dashboard
         } else {
-          showLoginErrorAlert();
+          showLoginErrorAlert(); // Mostrar alerta personalizada en caso de error
         }
       } catch (jsonError) {
         console.error('Error al parsear JSON:', jsonError);
@@ -65,19 +73,41 @@ const LoginScreen = ({ navigation, route }) => {
     }
   };
 
+  // Función para redirigir a la pantalla de registro
   const handleRegisterRedirect = () => {
     navigation.navigate('Register');
   };
 
+  // Función para redirigir a la pantalla de recuperación de contraseña
   const handleForgotPasswordRedirect = () => {
     navigation.navigate('PasswordRecovery');
   };
 
-  const clearLoginData = () => {
-    setUsername('');
-    setPassword('');
+  // Función para manejar el cierre de sesión
+  const handleLogout = async () => {
+    try {
+      const url = `${ip}/Expo_Comodo/api/services/public/cliente.php?action=logOut`;
+      console.log('URL solicitada:', url); // Verifica la URL en la consola
+
+      const response = await fetch(url, {
+        method: 'GET',
+      });
+
+      const data = await response.json();
+
+      if (data.status) {
+        Alert.alert('Sesión cerrada exitosamente');
+        navigation.navigate('Login');
+      } else {
+        Alert.alert('Error', data.error);
+      }
+    } catch (error) {
+      console.error('Error al cerrar sesión:', error);
+      Alert.alert('Error', 'Ocurrió un error al cerrar sesión');
+    }
   };
 
+  // Mostrar alerta de error de inicio de sesión
   const showLoginErrorAlert = () => {
     Alert.alert(
       'Error de inicio de sesión',
@@ -96,45 +126,50 @@ const LoginScreen = ({ navigation, route }) => {
     );
   };
 
-  useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
-      if (route.params?.clearLoginData) {
-        clearLoginData();
-      }
-    });
-
-    return unsubscribe;
-  }, [navigation, route.params]);
-
   return (
     <View style={styles.container}>
+      {/* Logo animado */}
       <Animated.Image
         source={require('../img/calentamiento.png')}
         style={[styles.logo, { transform: [{ translateY }] }]}
       />
+      {/* Título de la pantalla */}
       <Text style={styles.title}>Inicio de sesión</Text>
+      {/* Input para el nombre de usuario */}
       <TextInput
         style={styles.input}
         placeholder="Nombre de usuario"
         onChangeText={text => setUsername(text)}
         value={username}
       />
+      {/* Input para la contraseña */}
       <TextInput
         style={styles.input}
         placeholder="Contraseña"
         onChangeText={text => setPassword(text)}
         value={password}
-        secureTextEntry={true}
+        secureTextEntry={true} // Asegurar que la contraseña sea segura
       />
+      {/* Botón para iniciar sesión */}
       <Button3 style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>Iniciar sesión</Text>
       </Button3>
+      {/* Enlace para redirigir a la pantalla de registro */}
       <TouchableOpacity onPress={handleRegisterRedirect}>
         <Text style={styles.registerLink}>¿No tienes cuenta? Crea una</Text>
       </TouchableOpacity>
+      {/* Enlace para redirigir a la pantalla de recuperación de contraseña */}
       <TouchableOpacity onPress={handleForgotPasswordRedirect}>
         <Text style={styles.forgotPasswordText}>¿Olvidaste tu contraseña?</Text>
       </TouchableOpacity>
+      {/* Botón de cierre de sesión */}
+      <LogOut
+        title="Cerrar Sesión"
+        onPress={handleLogout}
+        style={styles.logoutButton}
+        textStyle={{ color: '#7f7f7f' }}
+        icon={<Ionicons name="lock-closed" size={24} color="black" />}
+      />
     </View>
   );
 };
