323db5b6910746c3c2c03cd31b91cd33b9654dd5
JV26vasr <veronicaro3101qr@gmail.com>
2024-08-13 10:39:34 -0600

    cambios en el carrito

--- src/screens/CarritoScreen.js
+++ src/screens/CarritoScreen.js
@@ -38,44 +38,49 @@ const CarritoScreen = ({ navigation }) => {
       fetchCarrito();
     }
   }, [isFocused, fetchCarrito]);
+
   const handleQuantityChange = async (item, type) => {
     let newCantidad = item.cantidad;
 
     if (type === 'increase') {
-        newCantidad++; // Permite incrementar sin límite
+      if (newCantidad >= 5) {
+        Alert.alert('Límite alcanzado', 'No puedes agregar más de 5 productos.');
+        return;
+      }
+      newCantidad++;
     } else if (type === 'decrease') {
-        newCantidad--;
+      newCantidad--;
     }
 
-    if (newCantidad < 1) return; // Evita que la cantidad sea menor que 1
+    if (newCantidad < 1) return;
 
     try {
-        const formData = new FormData();
-        formData.append('idDetalle', item.id_detalle_reserva.toString());
-        formData.append('cantidadProducto', newCantidad.toString());
+      const formData = new FormData();
+      formData.append('idDetalle', item.id_detalle_reserva.toString());
+      formData.append('cantidadProducto', newCantidad.toString());
 
-        const response = await fetch(`${ip}/Expo_Comodo/api/services/public/pedido.php?action=updateDetail`, {
-            method: 'POST',
-            body: formData,
-        });
+      const response = await fetch(`${ip}/Expo_Comodo/api/services/public/pedido.php?action=updateDetail`, {
+        method: 'POST',
+        body: formData,
+      });
 
-        const data = await response.json();
+      const data = await response.json();
 
-        if (data.status === 1) {
-            setCarrito(prevCarrito => (
-                prevCarrito.map(producto =>
-                    producto.id_detalle_reserva === item.id_detalle_reserva ? { ...producto, cantidad: newCantidad } : producto
-                )
-            ));
-            Alert.alert('Éxito', data.message);
-        } else {
-            Alert.alert('Error', data.error || 'Ocurrió un problema al actualizar la cantidad del producto');
-        }
+      if (data.status === 1) {
+        setCarrito(prevCarrito => (
+          prevCarrito.map(producto =>
+            producto.id_detalle_reserva === item.id_detalle_reserva ? { ...producto, cantidad: newCantidad } : producto
+          )
+        ));
+        Alert.alert('Éxito', data.message);
+      } else {
+        Alert.alert('Error', data.error || 'Ocurrió un problema al actualizar la cantidad del producto');
+      }
     } catch (error) {
-        Alert.alert('Error', 'Ocurrió un error al actualizar la cantidad del producto');
-        console.error(error);
+      Alert.alert('Error', 'Ocurrió un error al actualizar la cantidad del producto');
+      console.error(error);
     }
-};
+  };
   const handleDelete = async (idDetalle) => {
     try {
       const formData = new FormData();
